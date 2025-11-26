31a73b5dd3a3452469c5869c28300f80e6a1dc4c
MohammedObeid88 <mohammedobeid226@gmail.com>
2025-11-27 01:28:53 +0400

    Modifications

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,29 +1,30 @@
 from flask import Flask, request, jsonify, Blueprint
-from services.predictServices import getDiseasePred, getRecommend
+from app.services.predictServices import getDiseasePred, getRecommend, getYieldPred
+from app.dummy.dummyData import disease_data, recommendation_data, yield_data
 
 routes = Blueprint("routes", __name__)
 
 # predict_plant_health
-@routes.route('/api/predict_plant_health', methods=['POST'])
+@routes.route('/api/predict_plant_health', methods=['GET'])
 def predict_plant_health():
-    data = request.get_json()
+    data = disease_data
 
     if not data:
         return jsonify({'error': 'No input received'}), 400
     
     try:
-        prediction = getDiseasePred(data)
+        result = getDiseasePred(data)
         return jsonify({
             "model": "Random Forest",
-            "prediction": prediction
+            "result": result
         })
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
 # plant_recommendation
-@routes.route('/api/plant_recommendation', methods=['POST'])
+@routes.route('/api/plant_recommendation', methods=['GET'])
 def plant_recommendation():
-    data = request.get_json()
+    data = recommendation_data
 
     if not data:
         return jsonify({'error': 'No input received'}), 400
@@ -32,7 +33,24 @@ def plant_recommendation():
         result = getRecommend(data)
         return jsonify({
             "model": "XGBoost",
-            "recommendation": result
+            "result": result
+        })
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+# crop_yield
+@routes.route('/api/crop_yield', methods=['GET'])
+def crop_yield():
+    data = yield_data
+
+    if not data:
+        return jsonify({'error': 'No input received'}), 400
+    
+    try:
+        result = getYieldPred(data)
+        return jsonify({
+            "model": "XGBoost",
+            "result": result
         })
     except Exception as e:
         return jsonify({"error": str(e)}), 500
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-from api.routes import routes
+from app.api.routes import routes
 
 app = Flask(__name__)
 CORS(app)
