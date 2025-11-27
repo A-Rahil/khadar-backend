ec37cb6ba0d74f5f1c1c9d134a88d28d7b214943
A-Rahil <ahmrahil.cs@gmail.com>
2025-11-27 13:03:53 +0400

    Just a safety measure

--- app/services/report_service.py
+++ app/services/report_service.py
@@ -5,3 +5,9 @@ class HealthReport(FPDF):
         self.image()
         self.set_font('Arial', 'B', 15)
         self.cell(0, 10, 'Khadar - Plant Health Report')
+        self.ln(10)
+    def footer(self):
+        self.set_y(-15)
+        self.set_font('Arial', 'I', 8)
+        self.cell(0, 10, 'Page' + str(self.page_no()) + '{nb}', 0, 0, 'C')
+def generatePDFReport(farm)
